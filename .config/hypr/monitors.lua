542cda0552b544560338fec5c803ce493edec0a6
SatyaSB <satyzeus2327@gmail.com>
2026-07-02 02:05:59 +0530

    Hyprland configuration files added v0.1

---
+++ .config/hypr/monitors.lua
@@ -0,0 +1,13 @@
+-- [[
+┏┏ ┏━┃┏━ ┛━┏┛┏━┃┏━┃┏━┛
+┃┃┃┃ ┃┃ ┃┃ ┃ ┃ ┃┏┏┛━━┃
+┛┛┛━━┛┛ ┛┛ ┛ ━━┛┛ ┛━━┛
+]]
+hl.monitor({
+	output = "eDP-1",
+	disabled = false,
+	mode = "preferred",
+	scale = 2,
+	transform = 0,
+	cm = "hdr",
+})
